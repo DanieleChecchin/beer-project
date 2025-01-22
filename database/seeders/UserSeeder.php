d77f874b1dc6b172d728fec6c8d9dc11c9626d8d
DanieleChecchin <danielechecchin26@gmail.com>
2025-01-22 10:46:43 +0100

    hotfix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,16 +15,14 @@ class UserSeeder extends Seeder
     {
         $users = [
             [
-                'name' => 'Mario',
+                'first_name' => 'Mario',
                 'last_name' => 'Rossi',
-                'date_of_birth' => '1990-05-15',
                 'email' => 'mario.rossi@example.com',
                 'password' => 'password123',
             ],
             [
-                'name' => 'Anna',
+                'first_name' => 'Anna',
                 'last_name' => 'Bianchi',
-                'date_of_birth' => '1985-03-22',
                 'email' => 'anna.bianchi@example.com',
                 'password' => 'mypassword',
             ],
