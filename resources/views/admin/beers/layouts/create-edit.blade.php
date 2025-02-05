a63c8f852318c4278a9cf415b45a50b4663fb516
Beniamino-Soccio <beniamino43@gmail.com>
2025-02-05 11:53:17 +0100

    hotfiz color layout edit create

--- resources/views/admin/beers/layouts/create-edit.blade.php
+++ resources/views/admin/beers/layouts/create-edit.blade.php
@@ -15,43 +15,43 @@
                 </div>
 
                 <div class="mb-3">
-                    <label for="beer-name" class="form-label">beer name:</label>
-                    <input type="text" name="name" id="beer-name" class="form-control" value="{{ old("name", $beer->name) }}">
+                    <label for="beer-name" class="form-label text-white">beer name:</label>
+                    <input type="text" name="name" id="beer-name" class="form-control bg-dark text-white border-black" value="{{ old("name", $beer->name) }}">
                     {{-- @error("name")
                         @include("partials.single-name-error-message")
                     @enderror --}}
                 </div>
                 <div class="mb-3">
-                    <label for="beer-description" class="form-label">beer description:</label>
-                    <input type="text" name="description" id="beer-description" class="form-control" value="{{ old("description", $beer->description) }}">
+                    <label for="beer-description" class="form-label text-white">beer description:</label>
+                    <input type="text" name="description" id="beer-description" class="form-control bg-dark text-white border-black" value="{{ old("description", $beer->description) }}">
                     {{-- @error("name")
                         @include("partials.single-name-error-message")
                     @enderror --}}
                 </div>
                 <div class="mb-3">
-                    <label for="beer-type" class="form-label">beer type:</label>
-                    <input type="text" name="type" id="beer-type" class="form-control" value="{{ old("type", $beer->type) }}">
+                    <label for="beer-type" class="form-label text-white">beer type:</label>
+                    <input type="text" name="type" id="beer-type" class="form-control bg-dark text-white border-black" value="{{ old("type", $beer->type) }}">
                     {{-- @error("name")
                         @include("partials.single-name-error-message")
                     @enderror --}}
                 </div>
                 <div class="mb-3">
-                    <label for="beer-price" class="form-label">beer price:</label>
-                    <input type="text" name="price" id="beer-price" class="form-control" value="{{ old("price", $beer->price) }}">
+                    <label for="beer-price" class="form-label text-white">beer price:</label>
+                    <input type="text" name="price" id="beer-price" class="form-control bg-dark text-white border-black" value="{{ old("price", $beer->price) }}">
                     {{-- @error("name")
                         @include("partials.single-name-error-message")
                     @enderror --}}
                 </div>
                 <div class="mb-3">
-                    <label for="beer-alcohol_content" class="form-label">beer alcohol_content:</label>
-                    <input type="text" name="alcohol_content" id="beer-alcohol_content" class="form-control" value="{{ old("alcohol_content", $beer->alcohol_content) }}">
+                    <label for="beer-alcohol_content" class="form-label text-white">beer alcohol_content:</label>
+                    <input type="text" name="alcohol_content" id="beer-alcohol_content" class="form-control bg-dark text-white border-black" value="{{ old("alcohol_content", $beer->alcohol_content) }}">
                     {{-- @error("name")
                         @include("partials.single-name-error-message")
                     @enderror --}}
                 </div>
                 <div class="mb-3">
-                    <label for="beer-capacity" class="form-label">beer capacity:</label>
-                    <input type="text" name="capacity" id="beer-capacity" class="form-control" value="{{ old("capacity", $beer->capacity) }}">
+                    <label for="beer-capacity" class="form-label text-white">beer capacity:</label>
+                    <input type="text" name="capacity" id="beer-capacity" class="form-control bg-dark text-white border-black" value="{{ old("capacity", $beer->capacity) }}">
                     {{-- @error("name")
                         @include("partials.single-name-error-message")
                     @enderror --}}
